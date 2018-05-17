c1966da7712c2d6e1ffd5012a675b78cb225e102
john stockbauer <john@shippable.com>
2018-05-17 16:33:52 -0700

    try this

--- respond.sh
+++ respond.sh
@@ -4,4 +4,4 @@ fi
 
 response_payload="{\"response_type\": \"$response_type\", \"text\": \"$1\"}"
 
-curl -XPOST $response_url -H content-type:'application/json' -d "$response_payload"
+curl -XPOST "$response_url" -H content-type:'application/json' -d "$response_payload"
