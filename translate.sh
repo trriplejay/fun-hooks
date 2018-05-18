ee23789c1e7e5dd4b27c3eaf9b7f7d1f8906dbf5
John Stockbauer <john@shippable.com>
2018-05-17 22:00:48 -0700

    try new translate

--- translate.sh
+++ translate.sh
@@ -1,6 +1,11 @@
-api_url=http://api.funtranslations.com/translate/ermahgerd.json
+#!/bin/bash
 
 curl -XPOST $api_url \
   --data-urlencode "text=$message" > output.json
+
+curl -v  -i -XPOST \
+  -o output.json \
+  -d "text=$message" http://api.funtranslations.com/translate/ermahgerd.json
+
 
 cat output.json | jq '.contents.translated'
