e9e361abe453be6b980ab8ba6404941d0cbdc1c8
john stockbauer <john@shippable.com>
2018-05-17 17:02:39 -0700

    try translate rough

---
+++ translate.sh
@@ -0,0 +1,6 @@
+api_url=http://api.funtranslations.com/translate/ermahgerd.json
+
+curl -XPOST $api_url \
+  --data-urlencode "text=$message" > output.json
+
+cat output.json | jq '.contents.translated')
